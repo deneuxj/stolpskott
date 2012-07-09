caf7a0466dec9f9ca4f1c6ffa3f80a971126cfc1
johdex <johdex@localhost>
2012-07-09 23:36:53 +0200

    Added solver for scalar expressions.

--- MainGame/Script.fsx
+++ MainGame/Script.fsx
@@ -119,6 +119,52 @@ let dbg label f =
     printfn "Returned %A" r
     r
 
+let rec (|CleanS|) e =
+    match e with
+    | ScalarExpr.Add(CleanS (Constant 0.0), CleanS e)
+    | ScalarExpr.Add(CleanS e, CleanS (Constant 0.0)) ->
+        e
+
+    | ScalarExpr.Add(CleanS e1, CleanS e2) ->
+        e1 .+. e2
+
+    | Mult(CleanS(Constant 0.0), _)
+    | Mult(_, CleanS(Constant 0.0)) ->
+        Constant 0.0
+    
+    | Mult(CleanS(Constant 1.0), CleanS e)
+    | Mult(CleanS e, CleanS(Constant 1.0)) ->
+        e
+
+    | Mult(CleanS e1, CleanS e2) ->
+        e1 .*. e2
+
+    | Div(CleanS e, CleanS(Constant 1.0)) ->
+        e
+
+    | Div(CleanS (Constant 0.0), _) ->
+        Constant 0.0
+
+    | Div(CleanS e1, CleanS e2) ->
+        e1 ./. e2
+
+    | ScalarExpr.Variable _
+    | Constant _ -> e
+
+    | DotProduct(CleanV e1, CleanV e2) ->
+        DotProduct(e1, e2)
+
+    | ExtractX(CleanV e) -> ExtractX e
+    | ExtractY(CleanV e) -> ExtractY e
+    | ExtractZ(CleanV e) -> ExtractZ e
+
+and (|CleanV|) e =
+    match e with
+    | Build(CleanS e1, CleanS e2, CleanS e3) -> Build(e1, e2, e3)
+    | VectorExpr.Variable _ -> e
+    | VectorExpr.Add(CleanV e1, CleanV e2) -> e1 + e2
+    | Scale(CleanS k, CleanV e) -> Scale(k, e)
+
 let rec (|SumS|_|) e =
     fun () ->
         printfn "S: %A" e
@@ -153,6 +199,10 @@ let rec (|SumS|_|) e =
             None
     |> dbg "SumS"
 
+let sum es =
+    let (CleanS e) = List.fold (.+.) (Constant 0.0) es
+    e
+    
 let rec (|FactS|_|) v e =
     fun () ->
         printfn "F: %A" e
@@ -175,70 +225,62 @@ let rec (|FactS|_|) v e =
                 |> List.choose (fun e -> match e with FactS v e -> Some e | _ -> None)
 
             if List.length factEs = List.length es then
-                let sum =
-                    factEs
-                    |> List.fold (.+.) (Constant 0.0)
-                Some (sum)
+                Some (sum factEs)
             else
                 None
         | _ -> None
     |> dbg "FactS"
 
-let rec (|Clean|) e =
-    fun () ->
-        printfn "<<< %A" e
-        match e with
-        | ScalarExpr.Add(Clean (Constant 0.0), Clean e)
-        | ScalarExpr.Add(Clean e, Clean (Constant 0.0)) ->
-            e
+let rec containsS v e =
+    match e with
+    | ScalarExpr.Variable w when w = v ->
+        true
+    | ScalarExpr.Variable _ ->
+        false
+    | Constant _ ->
+        false
+    | ScalarExpr.Add(e1, e2)
+    | Div(e1, e2)
+    | Mult(e1, e2) ->
+        containsS v e1 || containsS v e2
+    | DotProduct(e1, e2) ->
+        containsV v e1 || containsV v e2
+    | ExtractX e
+    | ExtractY e
+    | ExtractZ e ->
+        containsV v e
 
-        | ScalarExpr.Add(Clean e1, Clean e2) ->
-            e1 .+. e2
-
-        | Mult(Clean(Constant 0.0), _)
-        | Mult(_, Clean(Constant 0.0)) ->
-            Constant 0.0
-    
-        | Mult(Clean(Constant 1.0), Clean e)
-        | Mult(Clean e, Clean(Constant 1.0)) ->
-            e
-
-        | Mult(Clean e1, Clean e2) ->
-            e1 .*. e2
-
-        | Div(Clean e, Clean(Constant 1.0)) ->
-            e
-
-        | Div(Clean (Constant 0.0), _) ->
-            Constant 0.0
-
-        | Div(Clean e1, Clean e2) ->
-            e1 ./. e2
-
-        | ScalarExpr.Variable _
-        | Constant _ -> e
-
-        | DotProduct _ -> e
-
-        | ExtractX _
-        | ExtractY _
-        | ExtractZ _ -> e
-
-    |> dbg "Clean"
-
-(*        
+and containsV v e =
+    match e with
+    | Build(e1, e2, e3) ->
+        containsS v e1 || containsS v e2 || containsS v e3
+    | VectorExpr.Variable _ ->
+        false
+    | VectorExpr.Add(e1, e2) ->
+        containsV v e1 || containsV v e2
+    | Scale(k, e) ->
+        containsS v k || containsV v e
+            
 let rec solveS v (lhs, rhs) =
     match lhs with
-    | Constant k -> [Constant 0.0, rhs .-. lhs]
-    | ScalarExpr.Variable w when w = v ->
-        [lhs, rhs]
-    | ScalarExpr.Variable _ ->
-        [Constant 0.0, rhs .-. lhs]
-    | ScalarExpr.Add (e1, e2) ->
-        failwith "TODO"
-    | Mult (e1, e2) ->
-        failwith "TODO"
+    | FactS v e -> Some (rhs ./. e)
+    | SumS(es) ->
+        let withV, withoutV =
+            es
+            |> List.partition (containsS v)
+        if List.isEmpty withoutV then
+            match lhs with
+            | SumS([SumS es1 ; SumS es2]) ->
+                let es = List.append es1 es2
+                solveS v (sum es, rhs)
+            | _ -> None
+        else
+            let lhs = sum withV
+            let rhs' = sum withoutV
+            solveS v (lhs, rhs .-. rhs')
+    | _ -> None     
 
+(*
 let solveV v e =
     let r1 = ExtractX e |> solveS v
     let r2 = ExtractY e |> solveS v
@@ -274,13 +316,8 @@ let v s = ScalarExpr.Variable s
 let n x = ScalarExpr.Constant x
 
 let e =
-    v "x" .+. (v "y" .*. (v "x" .+. n 0.0))
+    v "x" .+. (v "y" .*. (v "x" .+. n 1.0))
 
-let (Clean c) = (n 0.0 .+. n 1.0) .+. n 1.0
-
-printfn "%A" c
-
-match e with
-| FactS "x" (Clean e') -> printfn "%A = x * %A" e e'
-//| SumS es -> printfn "%A" es
-| _ -> printfn "???"
+match solveS "x" (e, Constant 0.0) with
+| Some(CleanS rhs) -> printfn "x = %A" rhs
+| None -> printfn "???"
